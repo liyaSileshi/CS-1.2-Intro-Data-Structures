5df2c9bf4e31c31db497811ecee39413ca48f570
liyaSileshi <liyasileshi@gmail.com>
2019-10-21 17:50:53 -0700

    rearrange module

---
+++ Code/rearrange.py
@@ -0,0 +1,11 @@
+import sys
+
+
+
+#https://stackoverflow.com/questions/4033723/how-do-i-access-command-line-arguments-in-python
+
+arg1 = sys.argv[1]
+arg2 = sys.argv[2]
+arg3 = sys.argv[3]
+
+print(arg1,arg2,arg3)
