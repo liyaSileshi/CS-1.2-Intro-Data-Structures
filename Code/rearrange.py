7bd6b695ffdff0082cfbf1c6414a81fdca0f2359
liyaSileshi <liyasileshi@gmail.com>
2019-10-23 16:36:44 -0700

    fixed rearrange.py

--- Code/rearrange.py
+++ Code/rearrange.py
@@ -2,22 +2,33 @@ import sys
 from random import choice
 
 
-#https://stackoverflow.com/questions/4033723/how-do-i-access-command-line-arguments-in-python
-args = sys.argv[1:]
+
 
 
 def random_rearrange(words):
+    print("words", words)
+    temp = words.copy()
+    print(temp)
     new_word = []
-    while len(words) > 0:
-       
-        pick = choice(words)
+    while len(temp) > 0:
+        print("words", words)
+        pick = choice(temp)
         new_word.append(pick)
-        words.remove(pick)
+        temp.remove(pick)
         # print(len(words))
-    
+    print("w", words)
     #https://bytes.com/topic/python/answers/794376-printing-contents-list-one-line
-    print(' '.join(item for item in new_word))
+    # print(' '.join(item for item in new_word))
+    return new_word
+    
+   
         
 
 if __name__ == '__main__':
-    random_rearrange(args)
+    #https://stackoverflow.com/questions/4033723/how-do-i-access-command-line-arguments-in-python
+    args = sys.argv[1:]
+    if len(args) >= 1 :
+        rand = random_rearrange(args)
+        print(' '.join(rand))
+    else:
+        print('not enough words')
