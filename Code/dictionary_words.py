accf81d259fefa32acd0c527cb2ab28c7bc466f3
liyaSileshi <liyasileshi@gmail.com>
2019-10-26 10:50:23 -0700

    fixed dictionary_words

--- Code/dictionary_words.py
+++ Code/dictionary_words.py
@@ -1,28 +1,37 @@
 import sys
 from random import choice
 
+#put it in a function
+
 #https://stackoverflow.com/questions/32470543/open-file-in-another-directory-python/32470564
 path = '/usr/share/dict/words'
-with open(path) as file:
-    
-    list = list(file)
-   
-    list = [w.replace('\n', '') for w in list]
-    # print(list)
-    # print(' '.join(item for item in list))
-    new = []
-
-    
-    # num = int(input('Enter how many words you want: '))
+def rand_dict(num):
+    with open(path) as file:
+        listr = list(file)
+        # listr = [w.replace('\n', '') for w in listr] 
+        listr = [word.strip() for word in listr] 
+        new = []
+        if num <= len(listr):
+            while num > 0:
+                pick = choice(listr)
+                new.append(pick)
+                num -= 1
+    return new
+        
+        
+if __name__ == '__main__':
+    #https://stackoverflow.com/questions/4033723/how-do-i-access-command-line-arguments-in-python
     num = int(sys.argv[1])
-   
-    if num <= len(list):
-        while num > 0:
-            pick = choice(list)
-            new.append(pick)
-            list.remove(pick)
-            num -= 1
-   
+    dict_words = rand_dict(num)
+    print(' '.join(dict_words)+'.')
 
-    print(' '.join(n for n in new)+'.')
-    
+
+
+
+# file = open(filename, 'r')
+# data= file.read()
+# lines = data.splitlines()
+
+# file.read().split('\n')
+
+
--- Code/rearrange.py
+++ Code/rearrange.py
@@ -1,34 +1,22 @@
 import sys
 from random import choice
 
-
-
-
-
 def random_rearrange(words):
-    print("words", words)
     temp = words.copy()
-    print(temp)
     new_word = []
     while len(temp) > 0:
-        print("words", words)
         pick = choice(temp)
         new_word.append(pick)
-        temp.remove(pick)
-        # print(len(words))
-    print("w", words)
-    #https://bytes.com/topic/python/answers/794376-printing-contents-list-one-line
-    # print(' '.join(item for item in new_word))
+        temp.remove(pick)  
     return new_word
     
-   
-        
-
 if __name__ == '__main__':
     #https://stackoverflow.com/questions/4033723/how-do-i-access-command-line-arguments-in-python
     args = sys.argv[1:]
     if len(args) >= 1 :
         rand = random_rearrange(args)
+        #https://bytes.com/topic/python/answers/794376-printing-contents-list-one-line
+        # print(' '.join(item for item in new_word))
         print(' '.join(rand))
     else:
         print('not enough words')
