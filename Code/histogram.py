8bf2ae19e7abc912280640a9addd3abfa5dd25d5
liyaSileshi <liyasileshi@gmail.com>
2019-10-27 16:03:41 -0700

    dictionary

--- Code/histogram.py
+++ Code/histogram.py
@@ -2,24 +2,49 @@ def histogram():
     with open('alice.txt') as file :
         words = file.read()
         split_words = words.split()
-        print(words)
-        print(split_words)
+       
+       #unique words
         unique_words = [] 
         for word in split_words:
             if word not in unique_words:
                 unique_words.append(word)
-       
-        print(unique_words)
+        # print(unique_words)
 
+        #frequency of each word
         word_freq = []
         for word in split_words :
             word_freq.append(split_words.count(word))
-        print(word_freq)
+        # print(word_freq)
+
+
+        #list
+        big_list = []
+        for word in split_words:
+            if [word,split_words.count(word)] not in big_list :
+                big_list.append([word, split_words.count(word)])
+        # print(big_list)
+
+
+
+        #dictionary
+        dictionary = []
+        for word in split_words:
+            if {word : split_words.count(word)} not in dictionary :
+                dictionary.append({word:split_words.count(word)})
+        print(dictionary)
+        
+
+        #tuple
+        big_tuple = []
+        for word in split_words:
+            if (word,split_words.count(word)) not in big_tuple :
+                big_tuple.append((word, split_words.count(word)))
+        print(big_tuple)
 
         #tuple
         #https://programminghistorian.org/en/lessons/counting-frequencies
         tuple_histogram = list(zip(split_words, word_freq))
-        print(tuple_histogram)
+        # print(tuple_histogram)
         unique_tuple = []
         for pairs in tuple_histogram :
             if pairs not in unique_tuple :
@@ -48,7 +73,8 @@ def frequency(word, histogram) :
             return i[1]
 
 
+
 hist = histogram()
 unique_words(hist)
-print(frequency('hi',hist))
+print(frequency('food',hist))
 
