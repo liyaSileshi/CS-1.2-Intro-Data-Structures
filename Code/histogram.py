c7321034dae7d0261c24db32d89e46456b65f18c
liyaSileshi <liyasileshi@gmail.com>
2019-10-30 17:06:55 -0700

    DRY code

--- Code/histogram.py
+++ Code/histogram.py
@@ -1,68 +1,62 @@
-def hist_dictionary():
+def list_of_words():
     with open('alice.txt') as file :
         words = file.read()
         split_words = words.split()
-        dictionary = {}
-        for word in split_words:
-            if word in dictionary :
-                dictionary[word] += 1
-            else :
-                dictionary[word] = 1
+        return split_words
+
+def hist_dictionary(words):
+    dictionary = {}
+    for word in words:
+        if word in dictionary :
+            dictionary[word] += 1
+        else :
+            dictionary[word] = 1
         #print(dictionary)
-        return dictionary
+    return dictionary
         
+def hist_list(words):
+    index = 0
+    count = 0
+    big_list = []
 
-def hist_list():
-    with open('alice.txt') as file :
-        words = file.read()
-        split_words = words.split()
-        index = 0
-        count = 0
-        big_list = []
+    #all words with a zero value
+    #when looping through split words, whenever the word exists,
+    #increase value by 1
+    for word in words:
+        if [word,count] not in big_list :
+            big_list.append([word,count])
 
-        #all words with a zero value
-        #when looping through split words, whenever the word exists,
-        #  increase value by 1
-        for word in split_words:
-            if [word,count] not in big_list :
-                big_list.append([word,count])
-
-        for word in split_words:
-            for i in range(len(big_list)) :
-                if big_list[i][index] == word :
-                    # print(word)
-                    big_list[i][1] += 1
-        print(big_list)
-        return big_list
+    for word in words:
+        for i in range(len(big_list)) :
+            if big_list[i][index] == word :
+                # print(word)
+                big_list[i][1] += 1
+    print(big_list)
+    return big_list
        
-def hist_tuple():
-    with open('alice.txt') as file :
-        words = file.read()
-        split_words = words.split()
-
-        #frequency of each word
-        word_freq = []
-        for word in split_words :
-            word_freq.append(split_words.count(word))
+def hist_tuple(words):
+    #frequency of each word
+    word_freq = []
+    for word in words:
+        word_freq.append(words.count(word))
         
-    
         #tuple
         big_tuple = []
-        for word in split_words:
-            if (word,split_words.count(word)) not in big_tuple :
-                big_tuple.append((word, split_words.count(word)))
+        for word in words:
+            if (word,words.count(word)) not in big_tuple :
+                big_tuple.append((word, words.count(word)))
         # print(big_tuple)
 
         #tuple
         #https://programminghistorian.org/en/lessons/counting-frequencies
-        tuple_histogram = list(zip(split_words, word_freq))
+        tuple_histogram = list(zip(words, word_freq))
         # print(tuple_histogram)
         unique_tuple = []
         for pairs in tuple_histogram :
             if pairs not in unique_tuple :
                 unique_tuple.append(pairs)
 
-def hist_counts_list():
+def hist_counts_list(words):
     with open('alice.txt') as file :
         words = file.read()
         split_words = words.split()
@@ -86,7 +80,7 @@ def hist_counts_list():
 
 def unique_words_dict(list) :
     count = 0
-    for key in list :
+    for _ in list :
         count += 1
     print(count)
     return count
@@ -111,17 +105,12 @@ def analysis(histogram):
     mean = sum/len(histogram)
     print(f'mean: {mean}')
 
-
-#What is the least/most frequent word(s)?
-#How many different words are used? unique_words(list)
-#What is the average (mean/median/mode) frequency of words in the text? 
 if __name__ == '__main__':
-    hist = hist_dictionary()
-    hist_list()
+    hist = hist_dictionary(list_of_words())
+    hist_list(list_of_words())
     # print(hist)
-
     unique_words_dict(hist)
     print(frequency_dict('two',hist))
     #analysis(hist)
     #write(hist)
-
+    
--- Code/random_hist.py
+++ Code/random_hist.py
@@ -1,30 +1,24 @@
 import random
-from histogram import hist_dictionary, hist_list
+from histogram import hist_dictionary, hist_list, list_of_words
+
+list_of_output = []
 
 def sample_by_frequency(histogram):
-    print(histogram)
-    hist_values = list(histogram.values())
-    sum = 0
-    for value in hist_values:
-        sum += value
-    #print(sum)
-    
-    rand = random.randrange(sum)
-    print(rand)
+    # hist_values = list(histogram.values())
+    tokens = sum(histogram.values())
+    rand = random.randrange(tokens)
     for key, value in histogram.items():
         if rand < value:
+            list_of_output.append(key)
             return key
         rand -= value
-    #print(fraction)
-    print(rand)
+  
+def test(sample):
+    for _ in range(10000):
+        sample_by_frequency(hist_dictionary(list_of_words()))
 
-    fraction = []
-    for value in hist_values:
-       frac = (value/sum)*100
-       fraction.append(frac)
- 
-
-if __name__ == '__main__':  
-
-    print(sample_by_frequency(hist_dictionary()))
+    return hist_dictionary(list_of_output)
    
+if __name__ == '__main__':  
+    print(test(sample_by_frequency(hist_dictionary(list_of_words()))))
+    print(sample_by_frequency(hist_dictionary(list_of_words())))
