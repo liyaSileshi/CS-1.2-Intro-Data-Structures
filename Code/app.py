27344b5085fc0264fc0911dbae981414e41f5e2e
liyaSileshi <liyasileshi@gmail.com>
2019-11-01 14:08:27 -0700

    fixing heroku

--- Code/app.py
+++ Code/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from dictionary_words import rand_dict
+import os
 app = Flask(__name__)
 
 @app.route('/')
@@ -8,3 +9,7 @@ def index():
     #print(' '.join(dict_words)+'.')
     # return rand_dict(5);
     return (' '.join(dict_words)+'.')
+
+
+if __name__ == '__main__':
+  app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
