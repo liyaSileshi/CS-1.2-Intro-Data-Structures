f0b46faca3c2e9460b72b4479d475cc05954f03e
liyaSileshi <liyasileshi@gmail.com>
2019-11-01 16:39:01 -0700

    procfile on root

--- Code/app.py
+++ Code/app.py
@@ -1,14 +1,17 @@
 from flask import Flask
 from dictionary_words import rand_dict
+from histogram import list_of_words
 import os
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    dict_words = rand_dict(6)
+    # dict_words = rand_dict(6)
     #print(' '.join(dict_words)+'.')
     # return rand_dict(5);
-    return (' '.join(dict_words)+'.')
+    # return (' '.join(dict_words)+'.')
+    return (' '.join(list_of_words())+'.')
+    
 
 
 if __name__ == '__main__':
