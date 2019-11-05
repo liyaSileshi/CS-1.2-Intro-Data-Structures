56d661d4898618fc0ee1f303b63fc13b0ef6e284
liyaSileshi <liyasileshi@gmail.com>
2019-11-05 11:24:30 -0800

    word_count

---
+++ Code/word_count.py
@@ -0,0 +1,20 @@
+import sys
+from tokenize import tokens
+def hist_dictionary(file_name):
+    """
+    Input: file name of the source text
+
+    Output: dictionary histogram 
+    """
+    list_of_words = tokens(file_name)
+    dictionary = {}
+    for word in list_of_words:
+        if word in dictionary :
+            dictionary[word] += 1
+        else :
+            dictionary[word] = 1
+    print(dictionary)
+    return dictionary
+
+if __name__ == '__main__':  
+    dictionary_histogram = hist_dictionary(sys.argv[1])
