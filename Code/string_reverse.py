51987429c2984bf2919e32068c1947bd90da729c
liyaSileshi <liyasileshi@gmail.com>
2019-11-05 09:14:35 -0800

    comments on string_reverse

--- Code/string_reverse.py
+++ Code/string_reverse.py
@@ -1,13 +1,18 @@
 import sys
 def reverse(word):
-    old_word = None
+    """
+    Input: takes in list of words
+
+    """
+    old_word = ""
     if len(word) == 1 :
+        #Joins the word from a list to a string
         old_word = ''.join(word)
     else:
         old_word = word
 
     new_word = []
-    reverse_word = None
+    reverse_word = ""
     while len(old_word) > 0:
         pick = old_word[len(old_word)-1]
         new_word.append(pick)
