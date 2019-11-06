7847e57d8a976d539f9e50faad436a163edb71e7
liyaSileshi <liyasileshi@gmail.com>
2019-11-06 13:08:54 -0800

    listogram works and passes all tests!

--- Code/listogram.py
+++ Code/listogram.py
@@ -26,8 +26,7 @@ class Listogram(list):
                 self[i][1] += count
                 self.tokens += count
                 return
-        # for i in range(len(self)) :
-        #     if word is not self[i][0]:
+        
         self.append([word, count])
         self.tokens += count
         self.types += 1
@@ -43,17 +42,30 @@ class Listogram(list):
     def __contains__(self, word):
         """Return boolean indicating if given word is in this histogram."""
         # TODO: Check if word is in this histogram
+        for i in range(len(self)) :
+            if word == self[i][0]:
+                return True
+        return False
 
     def index_of(self, target):
         """Return the index of entry containing given target word if found in
         this histogram, or None if target word is not found."""
         # TODO: Implement linear search to find index of entry with target word
+        for i in range(len(self)) :
+            if word == self[i][0]:
+                return i
+        return None
 
     def sample(self):
         """Return a word from this histogram, randomly sampled by weighting
         each word's probability of being chosen by its observed frequency."""
         # TODO: Randomly choose a word based on its frequency in this histogram
-
+        rand = random.randrange(self.tokens)
+        # for key, value in self.items():
+        for i in range(len(self)) :
+            if rand < self[i][1]:
+                return self[i][0]
+            rand -= self[i][1]
 
 def print_histogram(word_list):
     print()
