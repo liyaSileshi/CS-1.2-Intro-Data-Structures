7aba62c51692c3e0f73625a645af5db9dd8a2ea8
liyaSileshi <liyasileshi@gmail.com>
2019-11-08 15:32:21 -0800

    cleaning the script

--- Code/markov.py
+++ Code/markov.py
@@ -9,24 +9,13 @@ def markov():
     dict = {}
     word_list = []
     for i in range(len(words)-1):
-        # if words[i] in dict.keys() and dict[words[i]] not in word_list:
-            # word_list.append(dict[words[i]])
-            # print(word_list)
-            # dict[words[i]].append(words[i+1])
-        # if words[i+1] not in word_list:
-        #     word_list.append(words[i+1])
-        #     dict[words[i]] = word_list
-        
-        # else:
-        # else:
-            # dict[words[i]] = words[i+1]
-        # print(dict)
+       
         if words[i] not in dict.keys():
             word_list.append(words[i+1])
             dict[words[i]] = word_list
-        # else:
+        
         else:
-            # word_list.append()
+            
             dict[words[i]].append(words[i+1])
 
         word_list = []
