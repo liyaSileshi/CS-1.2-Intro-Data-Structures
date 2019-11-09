37172c81a59bd4ddbe9f09d8cece05dabf5973e1
liyaSileshi <liyasileshi@gmail.com>
2019-11-08 16:02:58 -0800

    nested dictionary works!!!!

--- Code/markov.py
+++ Code/markov.py
@@ -1,28 +1,36 @@
 from cleanup import clean
 import sys
 from tokenize import tokens
+import dictogram
 
 def markov():
+
     # clean('words.txt')
     words = tokens('words.txt')
     # print(words)
-    dict = {}
+    dict_word = {}
     word_list = []
     for i in range(len(words)-1):
        
-        if words[i] not in dict.keys():
+        if words[i] not in dict_word.keys():
             word_list.append(words[i+1])
-            dict[words[i]] = word_list
+            dict_word[words[i]] = word_list
         
         else:
-            
-            dict[words[i]].append(words[i+1])
-
+            dict_word[words[i]].append(words[i+1])
         word_list = []
 
+    nested_dict = {}
+    for key, value in dict_word.items():
+        dict_hist = dictogram.Dictogram(dict_word.get(key))
+        # value = dict_hist
+        nested_dict[key] = dict_hist
+        print(dict_hist)
 
-    print(dict)
-    print(word_list)
+    print(dict_word)
+    print(nested_dict)
+    # print(dict_word.)
+    # print(word_list)
 
 
 
