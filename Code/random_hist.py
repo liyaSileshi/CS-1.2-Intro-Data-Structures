a26dbb1fc84fb76b6f2718e5f30d4fb18692d25f
liyaSileshi <liyasileshi@gmail.com>
2019-11-09 12:50:41 -0800

    comment and docstrings on random_hist

--- Code/random_hist.py
+++ Code/random_hist.py
@@ -3,7 +3,11 @@ from histogram import hist_dictionary, hist_list, list_of_words
 import sys
 
 def sample_by_frequency(histogram):
-    # hist_values = list(histogram.values())
+    """
+    Input: Histogram of text file
+
+    Return : a random word based on the weight(occurence)
+    """
     tokens = sum(histogram.values())
     rand = random.randrange(tokens)
     for key, value in histogram.items():
@@ -12,12 +16,21 @@ def sample_by_frequency(histogram):
         rand -= value
   
 def test():
+    """
+    A test for sample_by_frequency histogram
+    """
     list_of_output = [] 
     words_list = list_of_words(sys.argv[1])
+    #change the text file to a dictionary histogram
     histogram = hist_dictionary(words_list)
+
+    #takes a random weighed sample from the histogram 1000x
+    #appends it to a list (list_of_output)
     for _ in range(1000):
         list_of_output.append(sample_by_frequency(histogram))
 
+    #changes the list_of_output list to a dictionary histogram
+    #prints out the result
     final = hist_dictionary(list_of_output)
     for key in final.keys():
         print(f"{key}: {final[key]}")
