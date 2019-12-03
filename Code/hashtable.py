670ca776fb7fd6bd304c945d60354234933d116f
liyaSileshi <liyasileshi@gmail.com>
2019-12-03 15:14:12 -0800

    using find method for hashtable methods

--- Code/hashtable.py
+++ Code/hashtable.py
@@ -23,10 +23,11 @@ class HashTable(object):
         # raise StopIteration
         # pass
         # curr = self.items()[0]
-        # for i in self.items():
-        #     return i
-        # raise StopIteration
+        for i in self.items():
+            yield i
+        raise StopIteration
         pass
+
     def __str__(self):
         """Return a formatted string representation of this hash table."""
         items = ['{!r}: {!r}'.format(key, val) for key, val in self.items()]
@@ -43,7 +44,10 @@ class HashTable(object):
 
     def keys(self):
         """Return a list of all keys in this hash table.
-        TODO: Running time: O(n2) Why and under what conditions?"""
+
+        Running time: O(n) for both best and average case since
+        we have to loop through each bucket and each list inside
+        the linked list to get the number of key-value entries."""
         # Collect all keys in each bucket
         all_keys = []
         for bucket in self.buckets:
@@ -53,9 +57,11 @@ class HashTable(object):
 
     def values(self):
         """Return a list of all values in this hash table.
-        TODO: Running time: O(???) Why and under what conditions?"""
-        # TODO: Loop through all buckets
-        # TODO: Collect all values in each bucket
+
+        Running time: O(n) for both best and average case since
+        we have to loop through each bucket and each list inside
+        the linked list to get the number of key-value entries"""
+
         all_values = []
         for bucket in self.buckets:
             for key,value in bucket.items():
@@ -64,7 +70,9 @@ class HashTable(object):
 
     def items(self):
         """Return a list of all items (key-value pairs) in this hash table.
-        TODO: Running time: O(???) Why and under what conditions?"""
+
+        Running time: O(n) since we have to go through each bucket and iterate
+        over the linked list as well then append it to a list."""
         # Collect all pairs of key-value entries in each bucket
         all_items = []
         for bucket in self.buckets:
@@ -74,42 +82,60 @@ class HashTable(object):
 
     def length(self):
         """Return the number of key-value entries by traversing its buckets.
-        TODO: Running time: O(???) Why and under what conditions?"""
+        Running time: O(1) Since it returns a value of size property"""
         return self.size
 
     def contains(self, key):
         """Return True if this hash table contains the given key, or False.
-        TODO: Running time: O(???) Why and under what conditions?"""
+
+        Running time: O(1) for best case, if the node is the first element
+        on the list. O(l) if the key does not exist or if it's in the last
+        item of the linked list. """
         try:
             self.get(key)
             return True
-
         except KeyError:
             return False
         
     def get(self, key):
         """Return the value associated with the given key, or raise KeyError.
-        TODO: Running time: O(???) Why and under what conditions?"""
+
+        Running time: O(1) if the key is the in the first item on the linked list.
+        O(l) if the key is the last item or if the item is not found. """
         index = self._bucket_index(key)
         ll = self.buckets[index]
-        # use find method
-        for buck_key, buck_value in ll.items():
-            if key is buck_key:
-                return buck_value
-        raise KeyError('Key not found: {}'.format(key))
-        # return False
+        node_data = ll.find(lambda item: item[0] == key)
+        if node_data is None:
+            raise KeyError('Key not found: {}'.format(key))
+        else:
+            return node_data[1]
+
+        # for buck_key, buck_value in ll.items():
+        #     if key is buck_key:
+        #         return buck_value
+        # raise KeyError('Key not found: {}'.format(key))
 
     def set(self, key, value):
         """Insert or update the given key with its associated value.
-        TODO: Running time: O(???) Why and under what conditions?"""
+
+        Running time: O(1) if the item to be replaced is the first
+        item in the linked list. O(l) if it is the last item in the linked list
+        since the find method has to traverse. Or if the item is not found as well O(l)"""
         index = self._bucket_index(key)
         ll = self.buckets[index]
-        for buck_key, buck_value in ll.items():
-            if key is buck_key:
-                ll.replace((buck_key,buck_value), (key, value))
-                return
-        ll.append((key, value))
-        self.size += 1
+        # for buck_key, buck_value in ll.items():
+        #     if key is buck_key:
+        # ll.replace((buck_key,buck_value), (key, value))
+        # return
+        node_data = ll.find(lambda item: item[0] == key)
+        if node_data == None:
+            ll.append((key, value))
+            self.size += 1
+
+        else:
+            ll.delete(node_data)
+            ll.append((key, value))
+
     def delete(self, key):
         """Delete the given key from this hash table, or raise KeyError.
         TODO: Running time: O(???) Why and under what conditions?"""
@@ -167,13 +193,13 @@ def test_hash_table():
 
 
 if __name__ == '__main__':
-    # test_hash_table()
-    ht = HashTable()
+    test_hash_table()
+    # ht = HashTable()
 
-    ht['liya'] = 'myself'
-    ht['josi'] = 'bro'
-    ht['boni'] = 'sis'
-    print(ht.items())
+    # ht['liya'] = 'myself'
+    # ht['josi'] = 'bro'
+    # ht['boni'] = 'sis'
+    # print(ht.items())
     # print(ht)
     # print(ht['liya'])
     # for i in ht.items():
