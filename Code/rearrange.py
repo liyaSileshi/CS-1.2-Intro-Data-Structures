f88903af41d864c65512ebffa671d6d667490f77
liyaSileshi <liyasileshi@gmail.com>
2019-12-04 00:12:41 -0800

    added fisher-yates for random rearrange

--- Code/rearrange.py
+++ Code/rearrange.py
@@ -1,5 +1,6 @@
 import sys
-from random import choice
+from random import choice, random
+import math
 
 def random_rearrange(words):
     """
@@ -15,13 +16,37 @@ def random_rearrange(words):
         temp.remove(pick)  
     return new_word
     
+def fisher_yates(words):
+    m = len(words)
+
+    # while there remain elements to shuffle
+    while m:
+        m -= 1
+        # pick a remaining
+        remain = math.floor(random() * m)
+        # And swap it with the current element
+        temp = words[m]
+        words[m] = words[remain]
+        words[remain] = temp
+
+    return words
+
+
 if __name__ == '__main__':
     #https://stackoverflow.com/questions/4033723/how-do-i-access-command-line-arguments-in-python
     args = sys.argv[1:]
     if len(args) >= 1 :
+        rand = fisher_yates(args)
+        #https://bytes.com/topic/python/answers/794376-printing-contents-list-one-line
+        # print(' '.join(item for item in new_word))
+        print('Fisher yates: '+' '.join(rand))
+    else:
+        print('not enough words')
+
+    if len(args) >= 1 :
         rand = random_rearrange(args)
         #https://bytes.com/topic/python/answers/794376-printing-contents-list-one-line
         # print(' '.join(item for item in new_word))
-        print(' '.join(rand))
+        print('Random_rearrange: '+' '.join(rand))
     else:
         print('not enough words')
