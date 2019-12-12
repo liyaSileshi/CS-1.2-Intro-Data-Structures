d36d5ad25d236e00b095451a3571353181cef22e
liyaSileshi <liyasileshi@gmail.com>
2019-12-12 11:52:23 -0800

    favorite db working

--- Code/app.py
+++ Code/app.py
@@ -2,20 +2,43 @@ from flask import Flask, render_template, request, jsonify
 import os
 # from markov import markov
 from markov_higher import Markov
-
+from pymongo import MongoClient
 import cleanup
 from tokenize_word import tokens
 import word_count
 import sample
 import sentence
 
+client = MongoClient()
+db = client.Tweet
+favorites = db.favorites
 app = Flask(__name__)
 
 @app.route('/', methods = ['GET', 'POST'])
 def index():
     """Desc: Home page for tweet bot"""
     """Displays random sentence based on weighed sample"""
+    if request.args.get('favorite') is not None:
+      print('favorite button')
+      # print(request.args.get('sentence'))
+      
     return render_template('index.html')
+
+
+@app.route('/favorites', methods = ['GET'])
+def favorite():
+  fav_sentence = request.args.get('sentence')
+  # { "sentence": "Hello world", "word_count": 10 }
+  # TODO: Save d to database... 
+  print(fav_sentence)
+  favorites.insert_one({'sentence': fav_sentence})
+  return
+  # return render_template('favorite.html', favorites = favorites)
+
+@app.route('/show_favorite')
+def show():
+  return render_template('favorite.html', favorites=favorites.find())
+
 
 @app.route("/data")
 def data():
@@ -27,9 +50,6 @@ def data():
   # elif book == 'b':
   #   words = tokens('bob.txt')
   markov = Markov(words, 3)
-  # if request.method == 'GET':
-  #       search_term = request.args.get('q')
-  #       print(search_term)
   sentence = markov.get_str(q).capitalize()
   sentence += '.'
   # histogram = word_count.hist_dictionary('words.txt')
--- Code/static/script.js
+++ Code/static/script.js
@@ -3,7 +3,9 @@ const slidesInner = slides.querySelector('.slides-inner')
 const text = document.getElementById('textSlide')
 const sub_button = document.getElementById('random_sentence')
 const num = rand.querySelector("input[type='number']")
-
+const sentence = document.querySelector('h3')
+const fav = document.getElementById('favorite')
+let json_sentence = ''
 
 sub_button.addEventListener('click', function(e){
   let q = 10
@@ -15,23 +17,42 @@ sub_button.addEventListener('click', function(e){
   fetch(`/data?q=${q}`).then(function(res) {
     return res.json()
   }).then(function(json) {
-    const h3 = document.querySelector('h3')
+    
     mySlides.nextSlide()
     // Use the JSON data to populate these elements
-    h3.innerHTML = json.sentence 
-    text.innerHTML = json.sentence
+    // console.log(json.sentence)
+    sentence.innerHTML = json.sentence 
+    json_sentence = json // { sentence: "Hello World" } 
+    // console.log(json_sentence)
+
   }).catch(function(err) {
     console.error(err.message)
   })
 })
 
+// async function foo(url){
+//   await console.log(json_sentence)
+//   console.log(url)
 
+// }
+
+fav.addEventListener('click', function(e){
+  console.log("It's getting clicked!")
+  console.log(json_sentence)
+
+  // JSON.stringify(json_sentence)
+  
+  const url = `/favorites?sentence=${json_sentence.sentence}`
+  fetch(url).then(function(res){
+    return res.json()
+  }).then(function(json){
+    console.log(json)
+  }).catch(function(err){
+    console.log(err.message)
+  })
+  
+  // foo(url).then()
+})
 
 const slideshow = document.querySelector('.ms-slide-show')
 const mySlides = makeSlideshow(slideshow)
-
-
-
-
-
-
